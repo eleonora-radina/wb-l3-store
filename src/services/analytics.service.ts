271769178e6f711eac71ec1f6f62eb085ca101c4
Eleonora Nikerina <eleonora.radina@mail.ru>
2023-12-16 02:56:33 +0400

    add analytics service and addToCard and purchase events

---
+++ src/services/analytics.service.ts
@@ -0,0 +1,22 @@
+export enum EventType {
+  Route = "route",
+  ViewCard = "viewCard",
+  ViewCardPromo = "viewCardPromo",
+  AddToCard = "addToCard",
+  Purchase = "purchase",
+}
+
+class AnalyticsService {
+  sendEvent (type: EventType, payload: any) {
+   return fetch('/api/sendEvent', {
+    method: 'POST',
+    body: JSON.stringify({
+      type: type,
+      payload: payload,
+      timestamp: Date.now()
+    })
+  });
+  }
+}
+
+export const analyticsService = new AnalyticsService();
