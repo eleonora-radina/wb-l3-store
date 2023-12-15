8204224f4bd91b8f7a3032766d07f2e29dd94229
Eleonora Nikerina <eleonora.radina@mail.ru>
2023-12-15 17:47:20 +0400

    add favourites' counter

--- src/services/favourites.service.ts
+++ src/services/favourites.service.ts
@@ -4,9 +4,10 @@ import { ProductData } from 'types';
 const DB = '__wb-favourites';
 
 class FavouriteService {
-  /*init() {
+  init() {
+    this._updCounters();
     this._displayFavBtn();
-  }*/
+  }
 
   async toggleProduct(product: ProductData) {
     if (!await this.isInFav(product)) {
@@ -32,8 +33,8 @@ class FavouriteService {
 
   async set(data: ProductData[]) {
     await localforage.setItem(DB, data);
-    //this._displayFavBtn();
-    //this._updCounters();
+    this._displayFavBtn();
+    this._updCounters();
   }
 
   async isInFav(product: ProductData) {
@@ -41,31 +42,28 @@ class FavouriteService {
     return products.some(({ id }) => id === product.id);
   }
 
-  /*private async _displayFavBtn() {
-    const products = await this.get();
-    const fav = document.querySelector('.fav');
-    if (products.length < 1) {
-      fav?.classList.remove('hide');
-    } else {
-      fav?.classList.add('hide');
-    }
-  }
-
-  init() {
-    this._updCounters();
-  }
-
-  async clear() {
-    await localforage.removeItem(DB);
-    this._updCounters();
-  }
-
   private async _updCounters() {
     const products = await this.get();
     const count = products.length >= 10 ? '9+' : products.length;
 
     //@ts-ignore
-    document.querySelectorAll('.js__cart-counter').forEach(($el: HTMLElement) => ($el.innerText = String(count || '')));
+    document.querySelectorAll('.js__fav-counter').forEach(($el: HTMLElement) => ($el.innerText = String(count || '')));
+  }
+
+  private async _displayFavBtn() {
+    const products = await this.get();
+    const fav = document.querySelector('.fav');
+    if (products.length < 1) {
+      fav?.classList.add('hide');
+    } else {
+      fav?.classList.remove('hide');
+    }
+  }
+
+  /*
+  async clear() {
+    await localforage.removeItem(DB);
+    this._updCounters();
   }*/
 }
 
