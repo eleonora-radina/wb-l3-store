d7d8bcc66e9df06e5ea3ea677ac7cbf96b578721
Eleonora Nikerina <eleonora.radina@mail.ru>
2023-12-22 16:21:10 +0400

    add viewCard event

--- src/services/analytics.service.ts
+++ src/services/analytics.service.ts
@@ -7,15 +7,22 @@ export enum EventType {
 }
 
 class AnalyticsService {
-  sendEvent (type: EventType, payload: any) {
-   return fetch('/api/sendEvent', {
-    method: 'POST',
-    body: JSON.stringify({
-      type: type,
-      payload: payload,
-      timestamp: Date.now()
-    })
-  });
+
+  sendEvent(type: EventType, payload: any) {
+    if (type === EventType.ViewCard) {
+      if (Object.entries(payload.product!.log).length !== 0) {
+        type = EventType.ViewCardPromo;
+      }
+    }
+
+    return fetch('/api/sendEvent', {
+      method: 'POST',
+      body: JSON.stringify({
+        type: type,
+        payload: payload,
+        timestamp: Date.now()
+      })
+    });
   }
 }
 
